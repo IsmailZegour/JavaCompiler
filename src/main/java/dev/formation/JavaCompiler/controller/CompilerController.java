b2ae35fc626a4b87456e0a97ccb86dc96a270bed
Ismail Zegour <ismail.zegour@gmail.com>
2024-11-28 13:01:34 +0100

    Structure temporaire

--- src/main/java/dev/formation/JavaCompiler/controller/CompilerController.java
+++ src/main/java/dev/formation/JavaCompiler/controller/CompilerController.java
@@ -2,25 +2,27 @@ package dev.formation.JavaCompiler.controller;
 
 import dev.formation.JavaCompiler.dto.CodeRequest;
 import dev.formation.JavaCompiler.dto.CodeResponse;
-import dev.formation.JavaCompiler.service.ICompilerService;
+import dev.formation.JavaCompiler.service.CompilerService;
 import dev.formation.JavaCompiler.validator.LanguageValidator;
 import dev.formation.JavaCompiler.validator.ValidatorFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping
 @CrossOrigin(origins = "${frontend.url}") // Autorise uniquement Angular
 public class CompilerController {
 
-    private final ICompilerService compilerService;
-
     private final ValidatorFactory validatorFactory;
 
-    public CompilerController(ICompilerService compilerService, ValidatorFactory validatorFactory) {
-        this.compilerService = compilerService;
+    private final CompilerService compilerService;
+
+    public CompilerController( ValidatorFactory validatorFactory,RestTemplate restTemplate,CompilerService compilerService) {
         this.validatorFactory = validatorFactory;
+        this.compilerService = compilerService;
     }
 
     @PostMapping("/compile")
@@ -28,13 +30,13 @@ public class CompilerController {
         try {
             String language = codeRequest.getLanguage();
             String code = codeRequest.getCode();
+
             LanguageValidator validator = validatorFactory.getValidator(language);
             validator.validate(code); // Validation avant traitement
 
-            String imageName = validator.getImageName();
             String instrumentedCode = validator.injectMeasurements(code);
 
-            return ResponseEntity.ok(compilerService.startContainer(instrumentedCode, imageName));
+            return ResponseEntity.ok(compilerService.compileAndExecute(instrumentedCode));
 
         } catch (IllegalArgumentException e) {
             return ResponseEntity
